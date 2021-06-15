3c06403d379b0f1b3f479fb4f6fe29f47758c6e6
nimmhn001 <85029169+nimmhn001@users.noreply.github.com>
2021-06-15 14:31:58 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,14 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+ int max(int a[], int s){
+
+        int maximum = a[0];
+        for(int i = 1; i < s; i++)
+        {
+            if(maximum < a[i])
+                maximum = a[i];
+        }
+
+        return maximum;
+    }
